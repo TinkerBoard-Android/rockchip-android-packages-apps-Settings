2b97957a26a54e527aad988be76083afc6b7a659
tzuwen_chang <TzuWen_Chang@asus.com>
2024-11-13 16:56:10 +0800

    adb: add a adb over ethernet item in developer options(1/2)
    Change-Id: Id0760fbeadcc217bc794b4dc4214bcf9438b6cc2

---
+++ src/com/android/settings/development/EthernetDebuggingPreferenceController.java
@@ -0,0 +1,96 @@
+package com.android.settings.development;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.SystemProperties;
+import android.provider.Settings;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.RestrictedLockUtils;
+import com.android.settingslib.RestrictedLockUtilsInternal;
+import com.android.settingslib.RestrictedSwitchPreference;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnPause;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+public class EthernetDebuggingPreferenceController extends DeveloperOptionsPreferenceController
+        implements Preference.OnPreferenceChangeListener, LifecycleObserver, OnResume, OnPause,
+        PreferenceControllerMixin {
+
+    private static final String PREFERENCE_KEY = "toggle_adb_ethernet";
+
+    @VisibleForTesting
+    static final int SETTING_VALUE_OFF = 0;
+    @VisibleForTesting
+    static final int SETTING_VALUE_ON = 1;
+
+    private RestrictedSwitchPreference mPreference;
+
+    public EthernetDebuggingPreferenceController(Context context, Lifecycle lifecycle) {
+        super(context);
+
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return PREFERENCE_KEY;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        updateState(mPreference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final boolean ethernetAdbSetting = (Boolean) newValue;
+
+        SystemProperties.set("persist.adb.tcp.port", ethernetAdbSetting ? "5555" : "");
+
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        String adbTcpPort = SystemProperties.get("persist.adb.tcp.port", "");
+
+        boolean isChecked = "5555".equals(adbTcpPort);
+        mPreference.setChecked(isChecked);
+    }
+
+    @Override
+    public void onResume() {
+        if (mPreference == null) {
+            return;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mPreference == null) {
+            return;
+        }
+    }
+
+    @Override
+    protected void onDeveloperOptionsSwitchDisabled() {
+        super.onDeveloperOptionsSwitchDisabled();
+        SystemProperties.set("persist.adb.tcp.port", "");
+        mPreference.setChecked(false);
+    }
+
+}
